dc1191dfd69d94b0a434ea74a5a4fd9b4425c2cd
arash mahdavi <arashmahdavi2007@gmail.com>
2025-04-05 23:43:15 +0330

    version 0.2, redis added

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"store/src/cache"
 	"store/src/configs"
 	"store/src/servers"
 )
 
 func main() {
 	cfg := configs.GetConfig()
+	cache.InitRedis()
+	defer cache.CloseRedis()
 	servers.NewServer(*cfg)
 }
